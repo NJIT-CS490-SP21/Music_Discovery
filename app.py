0862ce8ba5e91d9b4fc67de284779e5caacc7960
EC2 Default User <ec2-user@ip-172-31-17-183.ec2.internal>
2021-02-07 05:24:38 +0000

    Updated version, naming variable fixed

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route('/')
 def hello_world():
     
+    offset_url="https://tse2.mm.bing.net/th?id=OIP.vjqTJToCvwG4H00oH1Z4WgHaHa&pid=Api&P=0&w=300&h=300"
+    travis_url="https://tse3.mm.bing.net/th?id=OIP.NWMTwpG460SMaOxLd1SEKwHaEK&pid=Api&P=0&w=368&h=208"
+    future_url="https://i.redd.it/non7ml0cjsa41.png"
     song= top_song()
     return render_template(
         "index.html",
-        my_song_name=song['song_name'],
-        my_song_artist=song['song_artist'],
-        my_preview_url=song['preview_url'],
+        song_name=song['song_name'],
+        song_artist=song['song_artist'],
+        preview_url=song['preview_url'],
+        future=future_url,
+        travis=travis_url,
+        offset=offset_url,
         )
 
 app.run(
