77744dc1f3630aa1d478581da1867f8ffc3817f1
EC2 Default User <ec2-user@ip-172-31-17-183.ec2.internal>
2021-02-15 23:57:44 +0000

    song lyrics added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-from main import top_song
+from main import top_song,top_song_lyrics
 import os
 
 app=Flask(__name__)
@@ -7,19 +7,17 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def hello_world():
-    
-    offset_url="https://tse2.mm.bing.net/th?id=OIP.vjqTJToCvwG4H00oH1Z4WgHaHa&pid=Api&P=0&w=300&h=300"
-    travis_url="https://tse3.mm.bing.net/th?id=OIP.NWMTwpG460SMaOxLd1SEKwHaEK&pid=Api&P=0&w=368&h=208"
-    future_url="https://i.redd.it/non7ml0cjsa41.png"
     song= top_song()
+    song_artist=song['song_artist']
+    song_name=song['song_name']
+    lyrics=top_song_lyrics(song_artist,song_name)
     return render_template(
         "index.html",
         song_name=song['song_name'],
         song_artist=song['song_artist'],
         preview_url=song['preview_url'],
-        future=future_url,
-        travis=travis_url,
-        offset=offset_url,
+        song_img=song['song_img'],
+        song_lyrics_link=lyrics['song_lyrics_link']
         )
 
 app.run(
